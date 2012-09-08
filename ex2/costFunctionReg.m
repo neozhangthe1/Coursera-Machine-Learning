d492f9e1dee374ca1ed43e3853df4b41b2d043ea
Josh Gillespie <voxdeix@gmail.com>
2012-09-07 20:07:43 -0500

    Ex 2 part 2
    regularization work. Cost Functions and gradients.

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,10 +17,30 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+% non-regularized bits first
+h = sigmoid(X * theta);
+costPos = -y' * log(h);
+costNeg = (1 - y') * log(1 - h);
+% nonreg is equal to the non-regularized J in costFunction.m
+nonreg = (1/m) * (costPos - costNeg);
 
+% pop off the theta(1) param
+regTheta = theta(2:end, :);
+% calculate the regularization bit
+reg = (lambda / (2*m)) * (regTheta' * regTheta);
 
+% J is the the non-regularized cost plus the regularized parameters
+J = nonreg + reg;
 
+% g0 is equal to the non-regularized grad in costFunction.m
+g0 = (1/m) * (X' * (h - y));
+% get regularized bits
+gTheta = (lambda / m) * theta;
 
+% zero out the theta(1) term
+gTheta(1) = 0;
+
+grad = g0 + gTheta;
 
 % =============================================================
 
