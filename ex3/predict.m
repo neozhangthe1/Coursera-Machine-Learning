52748d745eb98e4c1f20e6e678c491f143baed67
Josh Gillespie <voxdeix@gmail.com>
2012-09-15 08:54:39 -0500

    just adding some comments

--- ex3/predict.m
+++ ex3/predict.m
@@ -24,20 +24,23 @@ p = zeros(size(X, 1), 1);
 % Theta2 has size 10 x 26
 % p has size 5000 x 1
 
+% Input Layer
 a1 = [ones(m, 1), X]; % results in [5000, 401]
 
 z2 = Theta1 * a1'; % results in [25, 5000]
 
+% Hidden Layer
 a2 = sigmoid(z2);  % results in [25, 5000]
 
 a2 = [ones(1, size(a2, 2)); a2]; % results in [26, 5000]
 
+% Output layer
 z3 = Theta2 * a2; % results in [10, 5000]
 
 a3 = sigmoid(z3); % results in [10, 5000]
 
-% calculating max on the transpose of a3 so the index result, p, 
-% has the expected dimensions [5000, 1]
+% calculating max on the transpose of a3 so the index 
+% result, p, has the expected dimensions [5000, 1]
 [val, p] = max(a3', [], 2);
 
 % =========================================================================
